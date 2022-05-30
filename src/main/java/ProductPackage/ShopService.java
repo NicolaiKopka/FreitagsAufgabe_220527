fea9559b7e2f893b1f3b3a54f63e11d5cde5f45c
Nicolai Kopka <nicolai.kopka@aol.de>
2022-05-30 17:05:49 +0200

    streams added

--- src/main/java/ProductPackage/ShopService.java
+++ src/main/java/ProductPackage/ShopService.java
@@ -13,22 +13,37 @@ public class ShopService {
         this.productRepo = productRepo;
     }
 
+//    public Product getProductById(String id) {
+//        Optional<Product> currentProduct = productRepo.getProductById(id);
+//        return currentProduct.orElseThrow(() -> new RuntimeException("Product does not exist"));
+//    }
+
+    // as Stream
     public Product getProductById(String id) {
-        Optional<Product> currentProduct = productRepo.getProductById(id);
-        return currentProduct.orElseThrow(() -> new RuntimeException("Product does not exist"));
+        return listProducts().stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst().orElseThrow(() -> new RuntimeException("Product does not exist"));
     }
 
     public List<Product> listProducts() {
         return productRepo.list();
     }
 
-    public void addOrder(List<String> idList) {
-        List<Product> productList = new ArrayList<>();
-        for(String id : idList) {
-            Product product = productRepo.getProductById(id).orElseThrow(() -> new RuntimeException("Product does not exist"));
-            productList.add(product);
+//    public void addOrder(List<String> idList) {
+//        List<Product> productList = new ArrayList<>();
+//        for(String id : idList) {
+//            Product product = productRepo.getProductById(id).orElseThrow(() -> new RuntimeException("Product does not exist"));
+//            productList.add(product);
+//
+//        }
+//        orderRepo.addOrder(productList);
+//    }
 
-        }
+        // as Stream
+        public void addOrder(List<String> idList) {
+        List<Product> productList = idList.stream()
+                .map(this::getProductById)
+                .toList();
         orderRepo.addOrder(productList);
     }
 
@@ -39,5 +54,11 @@ public class ShopService {
     public Order getOrderById(String id) {
         Optional<Order> currentOrder = orderRepo.getOrderById(id);
         return currentOrder.orElseThrow(() -> new RuntimeException("Order does not exist"));
+    }
+
+    public Product getProductsByName(String name) {
+        return listProducts().stream()
+                .filter(p -> p.getProductName().equals(name))
+                .findFirst().orElseThrow(() -> new RuntimeException("Product does not exist"));
     }
 }
